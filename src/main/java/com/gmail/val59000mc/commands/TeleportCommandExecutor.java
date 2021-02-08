ef724f44afe9491391fd98829d80347164c1c0da
Sirboys <khannanovalmaz1@gmail.com>
2021-02-08 19:59:47 +0500

    fix tp command

--- src/main/java/com/gmail/val59000mc/commands/TeleportCommandExecutor.java
+++ src/main/java/com/gmail/val59000mc/commands/TeleportCommandExecutor.java
@@ -6,6 +6,7 @@ import com.gmail.val59000mc.players.PlayerState;
 import com.gmail.val59000mc.players.UhcPlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -52,7 +53,9 @@ public class TeleportCommandExecutor implements CommandExecutor{
 			}
 
 			Location loc = new Location(player.getWorld(), x, y, z);
-			player.setSpectatorTarget(null);
+			if (player.getGameMode().equals(GameMode.SPECTATOR)) {
+				player.setSpectatorTarget(null);
+			}
 			player.teleport(loc);
 
 			player.sendMessage(Lang.COMMAND_SPECTATING_TELEPORT.replace("%player%", x + "/" + y + "/" + z));
@@ -70,7 +73,9 @@ public class TeleportCommandExecutor implements CommandExecutor{
 				sender.sendMessage(ChatColor.RED + "That player can not be found!");
 				return true;
 			}
-			player1.setSpectatorTarget(null);
+			if (player1.getGameMode().equals(GameMode.SPECTATOR)) {
+				player1.setSpectatorTarget(null);
+			}
 			player1.teleport(player2.getLocation());
 
 			player.sendMessage(Lang.COMMAND_SPECTATING_TELEPORT.replace("%player%", player1.getName()));
@@ -96,7 +101,9 @@ public class TeleportCommandExecutor implements CommandExecutor{
 		}
 
 		uhcPlayer.sendMessage(Lang.COMMAND_SPECTATING_TELEPORT.replace("%player%", uhcTarget.getName()));
-		player.setSpectatorTarget(null);
+		if (player.getGameMode().equals(GameMode.SPECTATOR)) {
+			player.setSpectatorTarget(null);
+		}
 		player.teleport(target);
 		return true;
 	}
